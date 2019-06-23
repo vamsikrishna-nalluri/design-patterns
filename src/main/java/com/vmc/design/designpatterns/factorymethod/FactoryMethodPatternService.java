cb6445a06c7938242d133bf4f6803e99be2990f7
Vamsi <vamckrishna.nalluri@gmail.com>
2019-06-23 16:30:40 +0530

    factory method pattern
    Added initial service class for factory method pattern.

---
+++ src/main/java/com/vmc/design/designpatterns/factorymethod/FactoryMethodPatternService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.vmc.design.designpatterns.factorymethod;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author krishna_v
+ *
+ */
+@Service
+public class FactoryMethodPatternService {
+	
+	FactoryMethodPatternService(){
+		//System.out.println("hello");
+	}
+	
+
+}
